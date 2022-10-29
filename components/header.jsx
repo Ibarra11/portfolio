9b62dfbcf1b8dd06ba1e6f30e9591ca0121b0cc2
Alan Ibarra <alan.ibarra209@gmail.com>
2022-10-28 17:45:31 -0700

    change the nav links depending on if there on the homepage

--- components/header.jsx
+++ components/header.jsx
@@ -1,93 +1,39 @@
-import { useEffect, useRef, useState, forwardRef } from "react";
 import Link from "next/link";
-
-function isCurrentSection(section, currentSection) {
-  return section === currentSection ? " text-light-emerald" : "";
-}
-export default function Header({ currentSection }) {
-  const navRef = useRef();
-  // useEffect(() => {
-  //   let previousScrollY = 0;
-  //   const appearAt = window.innerHeight * 0.9;
-  //   window.addEventListener("scroll", (e) => {
-  //     const navHeight = navRef.current.offsetHeight;
-  //     const direction = window.scrollY > previousScrollY ? "DOWN" : "UP";
-  //     if (
-  //       direction === "DOWN" &&
-  //       window.scrollY > navHeight &&
-  //       window.scrollY < appearAt
-  //     ) {
-  //       navRef.current.classList.remove("scale-y-100");
-  //       navRef.current.classList.add("scale-y-0");
-  //     } else if (direction === "DOWN" && window.scrollY > appearAt) {
-  //       navRef.current.classList.add("scale-y-100");
-  //     } else if (
-  //       direction === "UP" &&
-  //       window.scrollY < appearAt &&
-  //       window.scrollY > navHeight
-  //     ) {
-  //       navRef.current.classList.remove("scale-y-100");
-  //       navRef.current.classList.add("scale-y-0");
-  //     } else if (direction === "UP" && window.scrollY < navHeight) {
-  //       navRef.current.classList.remove("scale-y-0");
-  //       navRef.current.classList.add("scale-y-100");
-  //     }
-  //     previousScrollY = window.scrollY;
-  //   });
-  // }, []);
+import { useRouter } from "next/router";
+export default function Header() {
+  const router = useRouter();
   return (
     <header
       id="header"
       className="bg-black backdrop-blur-lg backdrop-filter fixed top-0  w-full z-20 opacity-90 h-16 duration-200 origin-top"
-      ref={navRef}
     >
       <nav className=" h-full">
         <ul className="h-full max-w-6xl mx-auto flex items-center text-light-grey">
           <li>
-            <Link href="#">
+            <Link href={`${router.pathname === "/" ? "#" : "/"}`}>
               <a className="p-3 pl-0">Logo</a>
             </Link>
           </li>
           <div className="ml-auto flex gap-8">
             <li>
-              <Link href="#">
-                <a className={`p-3${isCurrentSection("home", currentSection)}`}>
-                  Home
-                </a>
+              <Link href={`${router.pathname === "/" ? "#" : "/"}`}>
+                <a className="p-3">Home</a>
               </Link>
             </li>
             <li>
-              <Link href="#about">
-                <a
-                  className={`p-3${isCurrentSection("about", currentSection)}`}
-                >
-                  About Me
-                </a>
+              <Link href={`${router.pathname === "/" ? "#" : "/#about"}`}>
+                <a className="p-3">About Me</a>
               </Link>
             </li>
 
             <li>
-              <Link href="#projects">
-                <a
-                  className={`p-3${isCurrentSection(
-                    "projects",
-                    currentSection
-                  )}`}
-                >
-                  Projects
-                </a>
+              <Link href={`${router.pathname === "/" ? "#" : "/#projects"}`}>
+                <a className="p-3">Projects</a>
               </Link>
             </li>
             <li>
-              <Link href="#contact">
-                <a
-                  className={`p-3${isCurrentSection(
-                    "contact",
-                    currentSection
-                  )}`}
-                >
-                  Contact
-                </a>
+              <Link href={`${router.pathname === "/" ? "#" : "/#contact"}`}>
+                <a className="p-3">Contact</a>
               </Link>
             </li>
           </div>
