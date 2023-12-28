7fb2a239308cd9e0a9edb9a01c14f3344383e350
Ibarra11 <alanjibarradev@gmail.com>
2023-12-27 20:12:10 -0800

    add dynamic metadata

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,13 +1,29 @@
 import { readFileSync } from "fs";
 import ProjectDetail from "@/components/ProjectDetail";
-async function ProjectPage({ params }: { params: { slug: string } }) {
-  const json = readFileSync(`./content/${params.slug}.json`, {
+import React from "react";
+
+const getContent = React.cache((slug) => {
+  const json = readFileSync(`./content/${slug}.json`, {
     encoding: "utf-8",
   });
   const data = JSON.parse(json);
+  return data;
+});
+
+export async function generateMetadata({ params }) {
+  const content = getContent(params.slug);
+
+  return {
+    title: content.title,
+    description: content.description,
+  };
+}
+
+async function ProjectPage({ params }: { params: { slug: string } }) {
+  const content = getContent(params.slug);
   return (
     <div className=" bg-slate-800 pt-16 lg:pt-24 px-6">
-      <ProjectDetail {...data} />;
+      <ProjectDetail {...content} />;
     </div>
   );
 }
