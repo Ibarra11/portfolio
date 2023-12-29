eeb193cbae172bfd1f1adb4b68f0b5dbd481f7ae
Ibarra11 <alanjibarradev@gmail.com>
2023-12-28 19:56:35 -0800

    replace nextlink with link

--- components/MobileMenu.jsx
+++ components/MobileMenu.jsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import { TfiClose } from "react-icons/tfi";
 import { useRouter } from "next/navigation";
-import { flushSync } from "react-dom";
 
 const container = {
   hidden: { opacity: 0 },
@@ -51,7 +50,7 @@ export default function MobileMenu({ isOpen, handleOpenChange }) {
                   className="flex items-center flex-col gap-6 text-gray-200"
                 >
                   <motion.li variants={item}>
-                    <Link
+                    <a
                       onClick={() => {
                         handleOpenChange(false);
                       }}
@@ -59,10 +58,10 @@ export default function MobileMenu({ isOpen, handleOpenChange }) {
                       className="text-2xl px-12 py-2 hover:text-light-emerald duration-200 outline-none  focus:ring-4  focus:ring-light-emerald"
                     >
                       Home
-                    </Link>
+                    </a>
                   </motion.li>
                   <motion.li variants={item}>
-                    <Link
+                    <a
                       href="/#about"
                       onClick={() => {
                         handleOpenChange(false);
@@ -70,10 +69,10 @@ export default function MobileMenu({ isOpen, handleOpenChange }) {
                       className="text-2xl px-12 py-2 focus hover:text-light-emerald duration-200 outline-none  focus:ring-4  focus:ring-light-emerald"
                     >
                       About
-                    </Link>
+                    </a>
                   </motion.li>
                   <motion.li variants={item}>
-                    <Link
+                    <a
                       href="/#projects"
                       onClick={() => {
                         handleOpenChange(false);
@@ -81,10 +80,10 @@ export default function MobileMenu({ isOpen, handleOpenChange }) {
                       className="text-2xl px-12 py-2 hover:text-light-emerald duration-200 outline-none  focus:ring-4  focus:ring-light-emerald"
                     >
                       Projects
-                    </Link>
+                    </a>
                   </motion.li>
                   <motion.li variants={item}>
-                    <Link
+                    <a
                       href="/#contact"
                       onClick={() => {
                         handleOpenChange(false);
@@ -92,7 +91,7 @@ export default function MobileMenu({ isOpen, handleOpenChange }) {
                       className="text-2xl px-12 py-2 hover:text-light-emerald duration-200 outline-none  focus:ring-4  focus:ring-light-emerald"
                     >
                       Contact
-                    </Link>
+                    </a>
                   </motion.li>
                 </motion.ul>
                 <Dialog.Close
